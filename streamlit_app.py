ce478cab6939f182b1b724a76962d730ee45afc7
adwaid-10 <147416716+adwaid-10@users.noreply.github.com>
2024-03-29 11:50:14 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ app_mode = st.sidebar.selectbox("Select Page", ["Home", "About Project"])
 # Main Page
 if app_mode == "Home":
     st.markdown("<h2 style='text-align: center; color: white;'>CROP PREDICTION USING SOIL ANALYSIS </h2>", unsafe_allow_html=True)
-    st.image("Screenshot 2024-02-22 204925.png")
+    st.image("banner.png")
     st.subheader('Required Values', divider='rainbow')
     n = st.number_input("Nitrogen:", min_value=0,max_value=150,step=1)
     p = st.number_input("Phosphorus:", min_value=0,max_value=150, step=1)
@@ -30,7 +30,7 @@ if app_mode == "Home":
     temp = st.number_input("Temperature:", min_value=0,max_value=50,step=1)
     hum = st.number_input("Humidity:",min_value=0,max_value=100, step=1)
     ph = st.number_input("pH:",min_value=0,max_value=14,step=1)
-    rain = st.number_input("Rainfall:",min_value=20,max_value=350,step=1)
+    rain = st.number_input("Rainfall:",min_value=0,max_value=350,step=1)
 
     # Button for prediction
     if st.button("Predict"):
@@ -55,3 +55,20 @@ if app_mode == "Home":
             st.write("{} is a best crop to be cultivated ".format(crop))
         else:
             st.write("Sorry are not able to recommend a proper crop for this environment")
+if app_mode == "About Project":
+    st.header("About Project",divider="red")
+    st.subheader("About Dataset",divider='blue')
+    st.write("This dataset was build by augmenting datasets of rainfall, climate and fertilizer data available for India.")
+    st.markdown("##### Data fields")
+    st.markdown("* N - ratio of Nitrogen content in soil")
+    st.markdown("* P - ratio of Phosphorous content in soil")
+    st.markdown("* K - ratio of Potassium content in soil")
+    st.markdown("* temperature - temperature in degree Celsius")
+    st.markdown("* humidity - relative humidity in %")
+    st.markdown("* ph - ph value of the soil")
+    st.markdown("* rainfall - rainfall in mm")
+    st.text("Dataset taken from:")
+    st.code("https://www.kaggle.com/datasets/atharvaingle/crop-recommendation-dataset")
+    st.subheader("Project Objective",divider='blue')
+    st.write("Maximize agricultural yield by recommending appropriate crops")
+
